d404453faf173e0a68e167a8a4cda50d30ae71c5
ashtencodes <ashakemoto@gmail.com>
2022-04-11 10:14:05 -1000

    first commit, created weight class with datatypes and methods

---
+++ Weight.cpp
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file weight.cpp
+/// @version 1.0
+///
+/// @author @Ashten Akemoto <aakemoto@hawaii.edu>
+/// @date   11_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Weight.h"
